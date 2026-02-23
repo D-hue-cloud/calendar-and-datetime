5153cfacce704be0fac1b1408705535026c6b0a0
D-hue-cloud <dinugaratnayake@gmail.com>
2026-02-23 17:48:52 +0000

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,8 @@
+import datetime
+from datetime import date, time,  datetime
+today=date.today()
+now=datetime.now()
+print("Today is", today)
+print("Current date and time is", now)
+
+print("\nDate components: ", today, today.month, today.year)
---
+++ Activity2.py
@@ -0,0 +1,16 @@
+import random
+import time
+def GetRandomDate(startDate, endDate):
+    print("Printing Random datebetween between", startDate, "and", endDate)
+    randomGenerator=random.random()
+    dateFormat="%m/%d/%Y"
+
+    startTime=time.mktime(time.strptime(startDate, dateFormat))
+    endTime=time.mktime(time.strptime(endDate, dateFormat))
+
+    randomTime=startTime + randomGenerator + (endTime-startTime)
+    randomDate= time.strftime(dateFormat, time.localtime(randomTime))
+    return randomDate
+
+print("RandomDate=", GetRandomDate("2/23/2026", "3/23/2026"))
+
